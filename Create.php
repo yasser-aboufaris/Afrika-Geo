b9fa0c081dc4a23213044f8b44bf1a5b403566b2
yasser-aboufaris <azertyasser321@gmail.com>
2024-12-13 15:40:40 +0100

    affichage des pays de mysql

---
+++ Create.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class=" bg-slate-200 shadow-lg opacity-90 p-2">
+            <ul class="flex justify-around h-10">
+                <li class="font-bold text flex text-3xl hide ">
+                    <div class="text-yellow-500">GEO</div><span class="font-light">AFRIKA</span>
+                </li>
+                <li> <button class="  p-2 rounded-lg hover:scale-110" id="ShowFrom"> ADD CONTRY </button>
+                </li>
+                <li> <button class="  p-2 rounded-lg hover:scale-110" id="ShowFrom"> SHOW CONTRIES</button></li>
+            </ul>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    </header>
+</body>
+</html>
